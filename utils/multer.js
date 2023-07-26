32c4cd740bc8049b51f30abc4dc5ef4ff952d8b0
Nilshan Deemantha <ndeemantha1999@gmail.com>
2023-07-26 22:59:03 +0530

    Intial commit with feature  admin create user role

--- utils/multer.js
+++ utils/multer.js
@@ -5,10 +5,13 @@ const storage = multer.diskStorage({
       cb(null, 'uploads/'); // Specify the destination directory for uploaded files
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Customize the file name if needed
+      cb(null, Date.now() + '-' + file.originalname); 
+      // Customize the file name if needed
+      console.log("whoooooooooooooo")
     }
   });
 
 const upload = multer({ storage: storage });
+
 
 export { upload}
